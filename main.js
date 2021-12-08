36e69a2e6dae16fedd84688c5344ea38cc9d941f
DESKTOP-C2QNSMF\youn <sho801921@gmail.com>
2021-12-09 08:13:45 +0900

    WEB2 - Node.js(App) - 404 Not found 구현

--- main.js
+++ main.js
@@ -5,40 +5,42 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
+    // path와 pathname 차이
     var title = queryData.id;
-    if(_url == '/'){
-      title = 'Welcome';
+
+    if(pathname === '/'){
+      fs.readFile(`data/${queryData.id}`,'utf-8',function(err,description){
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>
+            ${description}
+            </p>
+          </body>
+          </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+      });
     }
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
+    else{
+      response.writeHead(404);
+      response.end('Not found');
     }
-    response.writeHead(200);
-    fs.readFile(`data/${queryData.id}`,'utf-8',function(err,description){
-      console.log(description)
-        var template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ul>
-          <h2>${title}</h2>
-          <p>
-          ${description}
-          </p>
-        </body>
-        </html>
-        `;
-      
-        response.end(template);
-    });
+
  
     //port번호를 입력(Why HOST(domain)에 여러 포트가 있을 수 있어서) + 기본값 80
     //http(protocol)://opentutorials.org(host or domain):3000(port)/main(path)?id=HTML&page=12(query String)
