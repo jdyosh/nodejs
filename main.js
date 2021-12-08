7700152b6f3c97e68fa9c33bd1b1dab433191b1d
DESKTOP-C2QNSMF\youn <sho801921@gmail.com>
2021-12-09 06:23:15 +0900

    WEB2 - Node.js - URL의 이해

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var fs = require('fs');
-var app = http.createServer(function(request,response){
+var app = http.createServer(function(request,response){ 
     var url = request.url;
     if(request.url == '/'){
       url = '/index.html';
@@ -15,4 +15,6 @@ var app = http.createServer(function(request,response){
     response.end(fs.readFileSync(__dirname + url));
     //response.end('egoing: '+url)
 });
+//port번호를 입력(Why HOST(domain)에 여러 포트가 있을 수 있어서) + 기본값 80
+//http(protocol)://opentutorials.org(host or domain):3000(port)/main(path)?id=HTML&page=12(query String)
 app.listen(3000);
