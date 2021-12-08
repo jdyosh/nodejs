cf4d165084bdc767df1ccc0e494b20b9e25cd46e
DESKTOP-C2QNSMF\youn <sho801921@gmail.com>
2021-12-09 08:26:17 +0900

    WEB2 - Node.js(App) - 홈페이지 구현(Home 분기)

--- main.js
+++ main.js
@@ -7,10 +7,12 @@ var app = http.createServer(function(request,response){
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
     // path와 pathname 차이
-    var title = queryData.id;
+
 
     if(pathname === '/'){
-      fs.readFile(`data/${queryData.id}`,'utf-8',function(err,description){
+      if(queryData.id === undefined){
+        var title = 'Welcome';
+        var description = 'Hi How are you doin?'
           var template = `
           <!doctype html>
           <html>
@@ -34,17 +36,45 @@ var app = http.createServer(function(request,response){
           `;
           response.writeHead(200);
           response.end(template);
-      });
+      }
+      else{
+          fs.readFile(`data/${queryData.id}`,'utf-8',function(err,description){
+            var title = queryData.id;
+            var template = `
+            <!doctype html>
+            <html>
+            <head>
+              <title>WEB1 - ${title}</title>
+              <meta charset="utf-8">
+            </head>
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              <ul>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=JavaScript">JavaScript</a></li>
+              </ul>
+              <h2>${title}</h2>
+              <p>
+              ${description}
+              </p>
+            </body>
+            </html>
+            `;
+            response.writeHead(200);
+            response.end(template);
+        });
+      }
     }
     else{
       response.writeHead(404);
       response.end('Not found');
     }
-
+  });
  
     //port번호를 입력(Why HOST(domain)에 여러 포트가 있을 수 있어서) + 기본값 80
     //http(protocol)://opentutorials.org(host or domain):3000(port)/main(path)?id=HTML&page=12(query String)
 
  
-});
+
 app.listen(3000);
