6dd505dfc30ae7c643ee65582de67322975a955c
sh6986 <okok960411@gmail.com>
2021-12-09 18:58:28 +0900

    WEB2 - Node.js / App - 글수정 - 수정할 정보 전송

--- WEB2-Nodejs/main.js
+++ WEB2-Nodejs/main.js
@@ -61,10 +61,10 @@ var app = http.createServer(function(request,response){
                 fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
                     var title = queryData.id;
                     var list = templateList(filelist);
-                    var template = templateHTML(title, list, `
-                        <h2>${title}</h2>${description}
-                        <a href="/create">create</a> <a href="/update?id=${title}">update</a>
-                    `);
+                    var template = templateHTML(title, list,
+                        `<h2>${title}</h2>${description}`,
+                        `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+                    );
 
                     response.writeHead(200);
                     response.end(template);
@@ -76,7 +76,7 @@ var app = http.createServer(function(request,response){
             var title = 'WEB - create';
             var list = templateList(filelist);
             var template = templateHTML(title, list, `
-                <form action="http://localhost:3000/create_process" method="post">
+                <form action="/create_process" method="post">
                     <p><input type="text" name="title" placeholder="title"></p>
                     <p>
                         <textarea name="description" placeholder="description"></textarea>
@@ -107,6 +107,31 @@ var app = http.createServer(function(request,response){
             });
         });
 
+    } else if (pathName === '/update') {
+        fs.readdir('./data', function (error, filelist) {
+            fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+                var title = queryData.id;
+                var list = templateList(filelist);
+                var template = templateHTML(title, list,
+                        `
+                            <form action="/update_process" method="post">
+                                <input type="hidden" name="id" value="${title}">
+                                <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+                                <p>
+                                    <textarea name="description" placeholder="description">${description}</textarea>
+                                </p>
+                                <p>
+                                    <input type="submit">
+                                </p>
+                            </form>
+                        `,
+                        `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+                    );
+
+                response.writeHead(200);
+                response.end(template);
+            });
+        });
     } else {
         response.writeHead(404);
         response.end('Not Found');
