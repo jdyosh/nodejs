7d164d19e19803e5fcb6fdf151ebbf865c80a221
sh6986 <okok960411@gmail.com>
2021-12-09 19:12:06 +0900

    WEB2 - Node.js / App - 글수정 - 수정된 내용 저장

--- WEB2-Nodejs/main.js
+++ WEB2-Nodejs/main.js
@@ -132,6 +132,25 @@ var app = http.createServer(function(request,response){
                 response.end(template);
             });
         });
+    } else if (pathName === '/update_process') {
+        var body = '';
+
+        request.on('data', function (data) {  // 데이터가 클 수 있으므로 조각조각 나눠서 전달.
+            body = body + data;
+        });
+        request.on('end', function () {  // 데이터가 너무 크면 꺼버리거나, 데이터를 다 전달하면 이 콜백함수 호출
+            var post = qs.parse(body);
+            var id = post.id;
+            var title = post.title;
+            var description = post.description;
+
+            fs.rename(`data/${id}`, `data/${title}`, function (error) {
+                fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+                    response.writeHead(302, {location: `/?id=${title}`});
+                    response.end();
+                });
+            });
+        });
     } else {
         response.writeHead(404);
         response.end('Not Found');
