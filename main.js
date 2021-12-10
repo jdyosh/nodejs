cee6c774e29f10f3a024ed14854b20127983275b
DESKTOP-C2QNSMF\youn <sho801921@gmail.com>
2021-12-10 13:27:16 +0900

    WEB2 - Node.js(App) - 출력정보에 대한 보안

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var qs = require('querystring');
 var template = require('./lib/template.js');
 //Object를 Module로 만들어서 사용(복잡성 축소)
 var path = require('path');
+var sanitizeHtml = require('sanitize-html');
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
@@ -39,14 +40,19 @@ var app = http.createServer(function(request,response){
             //console.log(fileList);
             var filteredId = path.parse(queryData.id).base
             fs.readFile(`data/${filteredId}`,'utf-8',function(err,description){
-              var title = queryData.id;             
+              var title = queryData.id;           
+              var sanitizedTitle = sanitizeHtml(title);
+              var sanitizedDescription = sanitizeHtml(description, {
+                allowedTags:['h1']
+              });
               var list = template.list(fileList);
+              console.log(sanitizedDescription)
               var html = template.html(title, list, 
-              `<h2>${title}</h2><p>${description}</p>`,
+              `<h2>${sanitizedTitle}</h2><p>${sanitizedDescription}</p>`,
               `<a href="/create">Create</a>
-               <a href="/update?id=${title}">Update</a>
+               <a href="/update?id=${sanitizedTitle}">Update</a>
                <form action="delete_process" method="post">
-                <input type="hidden" name="id" value="${title}">
+                <input type="hidden" name="id" value="${sanitizedTitle}">
                 <input type="submit" value="delete">
                </form>`);
               response.writeHead(200);
@@ -96,7 +102,8 @@ var app = http.createServer(function(request,response){
       fs.readdir('./data',function(error, fileList){
         var filteredId = path.parse(queryData.id).base
         fs.readFile(`data/${filteredId}`,'utf-8',function(err,description){
-          var title = queryData.id;             
+          var title = queryData.id;
+                       
           var list = template.list(fileList);
           var html = template.html(title, list, 
           `
@@ -119,35 +126,32 @@ var app = http.createServer(function(request,response){
     }
     else if(pathname === '/update_process'){
       var body = '';
-
       request.on('data', function(data){
-        body = body + data
-      }); 
-      request.on('end',function(){
-        var post = qs.parse(body);
-        //var id = post.id
-        var filteredId = path.parse(queryData.id).base
-        var title = post.title
-        var description = post.description
-        console.log(post)
-        fs.rename(`data/${filteredId}`, `data/${title}`, function(error){
-          fs.writeFile(`data/${title}`, description, 'utf-8', function(err){
-            response.writeHead(302, {Location: `/?id=${title}`});
-            response.end('Success');
+          body = body + data;
+      });
+      request.on('end', function(){
+          var post = qs.parse(body);
+          var id = post.id;
+          var title = post.title;
+          var description = post.description;
+          fs.rename(`data/${id}`, `data/${title}`, function(error){
+            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+              response.writeHead(302, {Location: `/?id=${title}`});
+              response.end();
+            })
           });
-        });
-      }); 
+      });
     }
     else if(pathname === '/delete_process'){
       var body = '';
-
       request.on('data', function(data){
         body = body + data
       }); 
+
       request.on('end',function(){
         var post = qs.parse(body);
-        //var id = post.id
-        var filteredId = path.parse(queryData.id).base
+        var id = post.id
+        var filteredId = path.parse(id).base
         fs.unlink(`data/${filteredId}`, function(error){
           response.writeHead(302, {Location: `/`});
           response.end('Success');
