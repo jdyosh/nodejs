ea32fc362e7b4409129e737daf322d2a66fac63b
sh6986 <okok960411@gmail.com>
2021-12-10 22:50:02 +0900

    Node.js - MySQL / MySQL로 글 수정 기능 구현

--- Node.js-MySQL/main.js
+++ Node.js-MySQL/main.js
@@ -21,7 +21,7 @@ var app = http.createServer(function(request,response){
     var pathname = url.parse(_url, true).pathname;
 
     if(pathname === '/') {
-        if (queryData.id === undefined) {
+        if (queryData.id === undefined) {           // 메인
             db.query(`SELECT * FROM topic`, function (error, topics) {
                 var title = 'Welcome';
                 var description = 'Hello, Node.js';
@@ -34,14 +34,14 @@ var app = http.createServer(function(request,response){
                 response.writeHead(200);
                 response.end(html);
             });
-        } else {
+        } else {            // 상세보기
             db.query(`SELECT * FROM topic`, function (error, topics) {
                 if (error) {
                     throw error;
                 }
 
                 db.query(`SELECT * FROM topic WHERE id = ?`, [queryData.id], function (error2, topic) {
-                    if (error) {
+                    if (error2) {
                       throw error2;
                     }
 
@@ -63,7 +63,7 @@ var app = http.createServer(function(request,response){
                 });
             });
         }
-    } else if (pathname === '/create') {
+    } else if (pathname === '/create') {            // 생성화면
         db.query(`SELECT * FROM topic`, function (error, topics) {
             var title = 'Create';
             var list = template.list(topics);
@@ -84,7 +84,7 @@ var app = http.createServer(function(request,response){
             response.end(html);
         });
 
-    } else if (pathname === '/create_process') {
+    } else if (pathname === '/create_process') {        // 생성
         var body = '';
 
         request.on('data', function(data){
@@ -105,48 +105,56 @@ var app = http.createServer(function(request,response){
             });
         });
 
-    } else if(pathname === '/update'){
-      fs.readdir('./data', function(error, filelist){
-        var filteredId = path.parse(queryData.id).base;
-        fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-          var title = queryData.id;
-          var list = template.list(filelist);
-          var html = template.HTML(title, list,
-            `
-            <form action="/update_process" method="post">
-              <input type="hidden" name="id" value="${title}">
-              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-              <p>
-                <textarea name="description" placeholder="description">${description}</textarea>
-              </p>
-              <p>
-                <input type="submit">
-              </p>
-            </form>
-            `,
-            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
-          );
-          response.writeHead(200);
-          response.end(html);
+    } else if (pathname === '/update') {        // 수정
+        db.query(`SELECT * FROM topic`, function (error, topics) {
+            if (error) {
+                throw error;
+            }
+
+            db.query(`SELECT * FROM topic WHERE id = ?`, [queryData.id], function (error2, topic) {
+                if (error2) {
+                    throw error2;
+                }
+
+                var list = template.list(topics);
+                var html = template.HTML(topic[0].title, list, `
+                    <form action="/update_process" method="post">
+                        <input type="hidden" name="id" value="${topic[0].id}">
+                        <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
+                        <p>
+                            <textarea name="description" placeholder="description">${topic[0].description}</textarea>
+                        </p>
+                        <p>
+                            <input type="submit">
+                        </p>
+                    </form>`, ''
+                );
+
+                response.writeHead(200);
+                response.end(html);
+            });
         });
-      });
-    } else if(pathname === '/update_process'){
-      var body = '';
-      request.on('data', function(data){
-          body = body + data;
-      });
-      request.on('end', function(){
-          var post = qs.parse(body);
-          var id = post.id;
-          var title = post.title;
-          var description = post.description;
-          fs.rename(`data/${id}`, `data/${title}`, function(error){
-            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-              response.writeHead(302, {Location: `/?id=${title}`});
-              response.end();
-            })
-          });
-      });
+    } else if(pathname === '/update_process'){      // 수정
+        var body = '';
+
+        request.on('data', function(data){
+            body = body + data;
+        });
+
+        request.on('end', function(){
+            var post = qs.parse(body);
+
+            db.query(`UPDATE topic SET title = ?, description = ?, author_id = 1 WHERE id = ?`,
+                [post.title, post.description, post.id], function (error, result) {
+                    if (error) {
+                        throw error;
+                    }
+
+                    response.writeHead(302, {Location: `/?id=${post.id}`});
+                    response.end();
+                });
+        });
+
     } else if(pathname === '/delete_process'){
       var body = '';
       request.on('data', function(data){
