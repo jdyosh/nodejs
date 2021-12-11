26a1c9194cec683f73ac65a6abeb1bc2f994a7c9
sh6986 <okok960411@gmail.com>
2021-12-12 00:56:33 +0900

    Node.js - Express / 삭제 기능 구현

--- SH/Node.js-Express/main.js
+++ SH/Node.js-Express/main.js
@@ -33,7 +33,7 @@ app.get('/page/:pageId', function (request, response) {
                 `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
                 ` <a href="/create">create</a>
                 <a href="/update/${sanitizedTitle}">update</a>
-                <form action="delete_process" method="post">
+                <form action="/delete_process" method="post">
                   <input type="hidden" name="id" value="${sanitizedTitle}">
                   <input type="submit" value="delete">
                 </form>`
@@ -116,74 +116,28 @@ app.post('/update_process', function (request, response) {
         var description = post.description;
         fs.rename(`data/${id}`, `data/${title}`, function(error){
             fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-                response.writeHead(302, {Location: `/page/${title}`});
-                response.end();
+                response.redirect(`/page/${title}`);
             })
         });
+    });
+});
+
+app.post('/delete_process', function (request, response) {
+    var body = '';
+    request.on('data', function(data){
+        body = body + data;
+    });
+    request.on('end', function(){
+        var post = qs.parse(body);
+        var id = post.id;
+        var filteredId = path.parse(id).base;
+        fs.unlink(`data/${filteredId}`, function(error){
+            response.redirect('/')
+        })
     });
 });
 
 app.listen(3000, function () {
     console.log(`Example app listening on port http://localhost:3000!`);
 })
-
-
-
-// var http = require('http');
-// var fs = require('fs');
-// var url = require('url');
-// var qs = require('querystring');
-// var template = require('./lib/template.js');
-// var path = require('path');
-// var sanitizeHtml = require('sanitize-html');
-//
-// var app = http.createServer(function(request,response){
-//     var _url = request.url;
-//     var queryData = url.parse(_url, true).query;
-//     var pathname = url.parse(_url, true).pathname;
-//     if(pathname === '/'){
-//       if(queryData.id === undefined){
-//       } else {
-//       }
-//     } else if(pathname === '/create'){
-//     } else if(pathname === '/create_process'){
-//     } else if(pathname === '/update'){
-//     } else if(pathname === '/update_process'){
-//       var body = '';
-//       request.on('data', function(data){
-//           body = body + data;
-//       });
-//       request.on('end', function(){
-//           var post = qs.parse(body);
-//           var id = post.id;
-//           var title = post.title;
-//           var description = post.description;
-//           fs.rename(`data/${id}`, `data/${title}`, function(error){
-//             fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-//               response.writeHead(302, {Location: `/?id=${title}`});
-//               response.end();
-//             })
-//           });
-//       });
-//     } else if(pathname === '/delete_process'){
-//       var body = '';
-//       request.on('data', function(data){
-//           body = body + data;
-//       });
-//       request.on('end', function(){
-//           var post = qs.parse(body);
-//           var id = post.id;
-//           var filteredId = path.parse(id).base;
-//           fs.unlink(`data/${filteredId}`, function(error){
-//             response.writeHead(302, {Location: `/`});
-//             response.end();
-//           })
-//       });
-//     } else {
-//       response.writeHead(404);
-//       response.end('Not found');
-//     }
-// });
-// app.listen(3000);
-
 
