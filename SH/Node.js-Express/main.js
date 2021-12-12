8ef026e2fc49bb1b28f37e74352b0ed1cbb54e54
sh6986 <okok960411@gmail.com>
2021-12-12 12:55:41 +0900

    Node.js - Express / 라우터

--- SH/Node.js-Express/main.js
+++ SH/Node.js-Express/main.js
@@ -1,12 +1,10 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
 var fs = require('fs');
-var template = require('./lib/template.js');
-var path = require('path');
-var sanitizeHtml = require('sanitize-html');
-var qs = require('querystring');
 var bodyParser = require('body-parser');
 var compression = require('compression');
+var indexRouter = require('./routes/index');
+var topicRouter = require('./routes/topic');
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: false}));
@@ -24,115 +22,8 @@ app.get('*', function (request, response, next) {  // 이 파라미터는 약속
 // 미들웨어가 필요 없는 요청들은 낭비이기 때문에 app.use를 app.get('*' 으로 바꿔서 get방식으로 들어오는 요청에만 적용할 수 있게 변경
 // => 모양이 만들어놨던 라우트코드와 같다. 따라서 라우트코드의 콜백함수는 미들웨어라고 할 수 있다.
 
-app.get('/', function (request, response) {
-    var title = 'Welcome';
-    var description = 'Hello, Node.js';
-    var list = template.list(request.list);
-    var html = template.HTML(title, list,
-        `<h2>${title}</h2>${description}
-                <img src="/images/hello.jpg" style="width: 300px; display:block; margin-top:10px;">`,
-        `<a href="/create">create</a>`
-    );
-    response.send(html);
-});
-
-app.get('/page/:pageId', function (request, response, next) {
-    var filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-        if (err) {
-            next(err);
-        } else {
-            var title = request.params.pageId;
-            var sanitizedTitle = sanitizeHtml(title);
-            var sanitizedDescription = sanitizeHtml(description, {
-                allowedTags:['h1']
-            });
-            var list = template.list(request.list);
-            var html = template.HTML(sanitizedTitle, list,
-                `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-                ` <a href="/create">create</a>
-                <a href="/update/${sanitizedTitle}">update</a>
-                <form action="/delete_process" method="post">
-                  <input type="hidden" name="id" value="${sanitizedTitle}">
-                  <input type="submit" value="delete">
-                </form>`
-            );
-            response.send(html);
-        }
-    });
-});
-
-app.get('/create', function (request, response) {
-    var title = 'WEB - create';
-    var list = template.list(request.list);
-    var html = template.HTML(title, list, `
-          <form action="/create_process" method="post">
-            <p><input type="text" name="title" placeholder="title"></p>
-            <p>
-              <textarea name="description" placeholder="description"></textarea>
-            </p>
-            <p>
-              <input type="submit">
-            </p>
-          </form>
-        `, '');
-    response.send(html);
-});
-
-app.post('/create_process', function (request, response) {
-    var post = request.body;
-    var title = post.title;
-    var description = post.description;
-    fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-        response.writeHead(302, {Location: `/page/${title}`});
-        response.end();
-    })
-});
-
-app.get('/update/:pageId', function (request, response) {
-    var filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-        var title = request.params.pageId;
-        var list = template.list(request.list);
-        var html = template.HTML(title, list,
-            `
-            <form action="/update_process" method="post">
-              <input type="hidden" name="id" value="${title}">
-              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-              <p>
-                <textarea name="description" placeholder="description">${description}</textarea>
-              </p>
-              <p>
-                <input type="submit">
-              </p>
-            </form>
-            `,
-            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
-        );
-        response.send(html);
-    });
-});
-
-app.post('/update_process', function (request, response) {
-    var post = request.body;
-    var id = post.id;
-    var title = post.title;
-    var description = post.description;
-    fs.rename(`data/${id}`, `data/${title}`, function(error){
-        fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-            response.redirect(`/page/${title}`);
-        })
-    });
-});
-
-app.post('/delete_process', function (request, response) {
-    var post = request.body;
-    var id = post.id;
-    var filteredId = path.parse(id).base;
-    fs.unlink(`data/${filteredId}`, function(error){
-        response.redirect('/')
-    })
-});
+app.use('/', indexRouter);
+app.use('/topic', topicRouter);
 
 app.use(function (request, response, next) {
     response.status(404).send('Sorry cant find that!');
