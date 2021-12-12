993306d7869fe79a93e4d4d84b1d82845a8eb672
sh6986 <okok960411@gmail.com>
2021-12-12 11:00:36 +0900

    Node.js - Express / 미들웨어 만들기

--- SH/Node.js-Express/main.js
+++ SH/Node.js-Express/main.js
@@ -12,52 +12,54 @@ app.use(bodyParser.urlencoded({extended: false}));
 // main.js가 실행되면서 app.use안에 해당 bodyParser 미들웨어가 들어오게 된다. 또한 실행된다.
 // 사용자가 전송한 post 데이터를 내부적으로 분석해서 request에 body 프로퍼티를 만든다. => request.body를 사용 가능하다.
 app.use(compression());
-
 // 미들웨어는 요청이 들어올 때 마다 실행된다. 요청이 들어올 때 마다 bodyParser가 실행되고 다음 compression이 실행된다.
 
+app.get('*', function (request, response, next) {  // 이 파라미터는 약속된것임.
+    fs.readdir('./data', function (error, filelist) {
+        request.list = filelist;  // request객체에 list라는 변수를 만들어서 filelist값을 담는다.
+        next();  // 그다음에 호출되어야 할 미들웨어가 담겨있다.
+    });
+});
+// 미들웨어가 필요 없는 요청들은 낭비이기 때문에 app.use를 app.get('*' 으로 바꿔서 get방식으로 들어오는 요청에만 적용할 수 있게 변경
+// => 모양이 만들어놨던 라우트코드와 같다. 따라서 라우트코드의 콜백함수는 미들웨어라고 할 수 있다.
 
 app.get('/', function (request, response) {
-    fs.readdir('./data', function(error, filelist){
-        var title = 'Welcome';
-        var description = 'Hello, Node.js';
-        var list = template.list(filelist);
-        var html = template.HTML(title, list,
-            `<h2>${title}</h2>${description}`,
-            `<a href="/create">create</a>`
-        );
-        response.send(html);
-    });
+    var title = 'Welcome';
+    var description = 'Hello, Node.js';
+    var list = template.list(request.list);
+    var html = template.HTML(title, list,
+        `<h2>${title}</h2>${description}`,
+        `<a href="/create">create</a>`
+    );
+    response.send(html);
 });
 
 app.get('/page/:pageId', function (request, response) {
-    fs.readdir('./data', function(error, filelist){
-        var filteredId = path.parse(request.params.pageId).base;
-        fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-            var title = request.params.pageId;
-            var sanitizedTitle = sanitizeHtml(title);
-            var sanitizedDescription = sanitizeHtml(description, {
-                allowedTags:['h1']
-            });
-            var list = template.list(filelist);
-            var html = template.HTML(sanitizedTitle, list,
-                `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-                ` <a href="/create">create</a>
+    var filteredId = path.parse(request.params.pageId).base;
+    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+        var title = request.params.pageId;
+        var sanitizedTitle = sanitizeHtml(title);
+        var sanitizedDescription = sanitizeHtml(description, {
+            allowedTags:['h1']
+        });
+        var list = template.list(request.list);
+        var html = template.HTML(sanitizedTitle, list,
+            `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+            ` <a href="/create">create</a>
                 <a href="/update/${sanitizedTitle}">update</a>
                 <form action="/delete_process" method="post">
                   <input type="hidden" name="id" value="${sanitizedTitle}">
                   <input type="submit" value="delete">
                 </form>`
-            );
-            response.send(html);
-        });
+        );
+        response.send(html);
     });
 });
 
 app.get('/create', function (request, response) {
-    fs.readdir('./data', function(error, filelist){
-        var title = 'WEB - create';
-        var list = template.list(filelist);
-        var html = template.HTML(title, list, `
+    var title = 'WEB - create';
+    var list = template.list(request.list);
+    var html = template.HTML(title, list, `
           <form action="/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p>
@@ -68,8 +70,7 @@ app.get('/create', function (request, response) {
             </p>
           </form>
         `, '');
-        response.send(html);
-    });
+    response.send(html);
 });
 
 app.post('/create_process', function (request, response) {
@@ -83,13 +84,12 @@ app.post('/create_process', function (request, response) {
 });
 
 app.get('/update/:pageId', function (request, response) {
-    fs.readdir('./data', function(error, filelist){
-        var filteredId = path.parse(request.params.pageId).base;
-        fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-            var title = request.params.pageId;
-            var list = template.list(filelist);
-            var html = template.HTML(title, list,
-                `
+    var filteredId = path.parse(request.params.pageId).base;
+    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+        var title = request.params.pageId;
+        var list = template.list(request.list);
+        var html = template.HTML(title, list,
+            `
             <form action="/update_process" method="post">
               <input type="hidden" name="id" value="${title}">
               <p><input type="text" name="title" placeholder="title" value="${title}"></p>
@@ -101,10 +101,9 @@ app.get('/update/:pageId', function (request, response) {
               </p>
             </form>
             `,
-                `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
-            );
-            response.send(html);
-        });
+            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+        );
+        response.send(html);
     });
 });
 
