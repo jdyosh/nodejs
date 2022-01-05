6492b6b4d89bd25cf2007e2e1f52b7d08e115b20
sh6986 <okok960411@gmail.com>
2022-01-05 12:26:51 +0900

    Node.js - 쿠키와 인증 / Secure & HttpOnly

--- SH/Node.js-Cookie/nodejs/cookie.js
+++ SH/Node.js-Cookie/nodejs/cookie.js
@@ -11,12 +11,14 @@ http.createServer(function (request, response) {
     console.log(cookies.yummy_cookie);
 
     // 쿠키 생성
-    // response.writeHead(200, {
-    //     'Set-Cookie': [
-    //         'yummy_cookie=choco',
-    //         'tasty_cookie=strawberry',
-    //         `Permanent=cookies; Max-Age=${60*60*24*30}`
-    //     ]
-    // });
+    response.writeHead(200, {
+        'Set-Cookie': [
+            'yummy_cookie=choco',
+            'tasty_cookie=strawberry',
+            `Permanent=cookies; Max-Age=${60*60*24*30}`,
+            'Secure=Secure; Secure',
+            'HttpOnlytest=HttpOnlytest; HttpOnly'
+        ]
+    });
     response.end('Cookie!!');
 }).listen(3000);
