29d510c1f42e2b68aa68ba8f6c7e67cca986fe56
sh6986 <okok960411@gmail.com>
2022-01-08 22:30:54 +0900

    Node.js - passport.js / passport.js 자격확인

--- SH/passport.js/main.js
+++ SH/passport.js/main.js
@@ -20,15 +20,50 @@ app.use(session({
   store: new FileStore()
 }));
 
+var authData = {
+    email: 'egoing@gmail.com',
+    password: '1111',
+    nickname: 'egoing'
+};
+
 // passport 관련
 // passport는 세션을 내부적으로 사용하므로 express-session을 활성화시키는 코드 다음에 와야 한다.
 var passport = require('passport')
     , LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+    {   // form 에서 아이디 비밀번호를 넘겨줄때 username, password 라는 name으로 넘겨야 하는데 다른이름으로 넘기고 싶을때
+        usernameField: 'email',
+        passwordField: 'pwd'
+    },
+    function (username, password, done) {   // done이라는 함수를 어떻게 호출하냐에 따라 성공실패를 알려준다
+        console.log('LocalStrategy', username, password);
+        if (username === authData.email) {
+            console.log(1);
+            if (password === authData.password) {
+                console.log(2);
+                return done(null, authData);
+            } else {
+                console.log(3);
+                return done(null, false, {
+                    message: 'Incorrect password.'
+                });
+            }
+        } else {
+            console.log(4);
+            return done(null, false, {
+                message: 'Incorrect username.'
+            });
+        }
+    }
+));
+
 app.post('/auth/login_process',
     passport.authenticate('local', {  // local: username과 password로 로그인하는것, local이 아닌 그외방식은 facebook, google등으로 로그인 하는것
       successRedirect: '/', // 성공시
       failureRedirect: '/auth/login' // 실패시
     }));
+
 
 
 app.get('*', function(request, response, next){
