28e6e28ac08dd79d6c90e822ff097b60ea66ad7f
sh6986 <okok960411@gmail.com>
2022-01-08 21:41:46 +0900

    Node.js - passport.js / passport.js 설치

---
+++ SH/passport.js/main.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var compression = require('compression');
+var helmet = require('helmet');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+
+app.use(helmet());
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(compression());
+
+app.use(session({
+  secret: 'asdfgg',
+  resave: false,
+  saveUninitialized: true,
+  store: new FileStore()
+}));
+
+// passport 관련
+// passport는 세션을 내부적으로 사용하므로 express-session을 활성화시키는 코드 다음에 와야 한다.
+var passport = require('passport')
+    , LocalStrategy = require('passport-local').Strategy;
+
+app.get('*', function(request, response, next){
+  fs.readdir('./data', function(error, filelist){
+    request.list = filelist;
+    next();
+  });
+});
+
+var indexRouter = require('./routes');
+var topicRouter = require('./routes/topic');
+var authRouter = require('./routes/auth');
+
+app.use('/', indexRouter);
+app.use('/topic', topicRouter);
+app.use('/auth', authRouter);
+
+app.use(function(req, res, next) {
+  res.status(404).send('Sorry cant find that!');
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+});
+
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!')
+});
