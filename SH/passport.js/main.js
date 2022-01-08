a2201e117955129fa8d20a62baa9e2e72a38d40f
sh6986 <okok960411@gmail.com>
2022-01-08 23:18:13 +0900

    Node.js - passport.js / passport.js 세션이용

--- SH/passport.js/main.js
+++ SH/passport.js/main.js
@@ -15,9 +15,9 @@ app.use(compression());
 
 app.use(session({
   secret: 'asdfgg',
-  resave: false,
-  saveUninitialized: true,
-  store: new FileStore()
+  resave: false, // true로 할경우 session을 미리 만드는것이도 false로 할경우는 session을 저장할때에 생성되게 한것입니다.
+  saveUninitialized: false,
+  store: new FileStore({path:'./sessions',logFn:function(){}})
 }));
 
 var authData = {
@@ -31,6 +31,20 @@ var authData = {
 var passport = require('passport')
     , LocalStrategy = require('passport-local').Strategy;
 
+app.use(passport.initialize());  // passport 미들웨어를 express에 설치
+app.use(passport.session());    // 내부적으로 passport는 세션 위에서 동작하므로
+
+passport.serializeUser(function(user, done) {   // 로그인에 성공했을 때 딱 한번 호출한다.
+    console.log('serializeUser', user);
+    // 세션 데이터 안에 passport user 값으로 사용자의 식별자가 저장된다.
+    done(null, user.email);  // 두번째인자는 구분되는 식별자.
+});
+
+passport.deserializeUser(function (id, done) { // 페이지를 방문할 때 마다 호출된다. 로그인한 사용자인지 아닌지 체크할때 사용한다.
+    console.log('deserializeUser', id);
+    done(null, authData);
+});
+
 passport.use(new LocalStrategy(
     {   // form 에서 아이디 비밀번호를 넘겨줄때 username, password 라는 name으로 넘겨야 하는데 다른이름으로 넘기고 싶을때
         usernameField: 'email',
@@ -39,18 +53,14 @@ passport.use(new LocalStrategy(
     function (username, password, done) {   // done이라는 함수를 어떻게 호출하냐에 따라 성공실패를 알려준다
         console.log('LocalStrategy', username, password);
         if (username === authData.email) {
-            console.log(1);
             if (password === authData.password) {
-                console.log(2);
-                return done(null, authData);
+                return done(null, authData);    // 바로 다음에 passport.serializeUser를 호출한다.
             } else {
-                console.log(3);
                 return done(null, false, {
                     message: 'Incorrect password.'
                 });
             }
         } else {
-            console.log(4);
             return done(null, false, {
                 message: 'Incorrect username.'
             });
@@ -63,7 +73,7 @@ app.post('/auth/login_process',
       successRedirect: '/', // 성공시
       failureRedirect: '/auth/login' // 실패시
     }));
-
+// / passport 관련
 
 
 app.get('*', function(request, response, next){
