97690fd749ae51d959e69b79cd2318073dc8c34b
sh6986 <okok960411@gmail.com>
2022-01-08 21:52:28 +0900

    Node.js - passport.js / passport.js 인증 구현

--- SH/passport.js/main.js
+++ SH/passport.js/main.js
@@ -24,6 +24,12 @@ app.use(session({
 // passport는 세션을 내부적으로 사용하므로 express-session을 활성화시키는 코드 다음에 와야 한다.
 var passport = require('passport')
     , LocalStrategy = require('passport-local').Strategy;
+app.post('/auth/login_process',
+    passport.authenticate('local', {  // local: username과 password로 로그인하는것, local이 아닌 그외방식은 facebook, google등으로 로그인 하는것
+      successRedirect: '/', // 성공시
+      failureRedirect: '/auth/login' // 실패시
+    }));
+
 
 app.get('*', function(request, response, next){
   fs.readdir('./data', function(error, filelist){
